103f4957c5fc9ffc193a93a12205237672256ff5
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-12-10 00:25:16 -0500

    update: new test implementation

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import argparse
-import torch
-import cv2
 from src.deep_q_network import DeepQNetwork
 from src.flappy_bird import FlappyBird
 from src.utils import pre_processing
+import argparse
+import torch
+import cv2
 
 image_size = 84
 saved_path = "trained_models"
@@ -16,22 +16,21 @@ def test_flap(opt):
 
     model = torch.load("{}/flappy_bird_final_newnn".format(saved_path), map_location=lambda storage, loc: storage)
     model.eval()
-    game_state = FlappyBird()
-    image, reward, terminal = game_state.next_frame(0)
-    image = pre_processing(image[:game_state.screen_width :int(game_state.base_y)], image_size, image_size)
-    image = torch.from_numpy(image)
+    fb = FlappyBird()
+    init_image, reward, stop = fb.next_frame(0)
+    init_image = pre_processing(init_image[:fb.screen_width :int(fb.ground_y)], image_size, image_size)
+    init_image = torch.from_numpy(init_image)
     if torch.cuda.is_available():
         model.cuda()
-        image = image.cuda()
-    state = torch.cat(tuple(image for _ in range(4)))[None, :, :, :]
+        init_image = init_image.cuda()
+    state = torch.cat(tuple(image for i in range(4)))[None, :, :, :]
 
-    while 1:
+    while True:
+        pred = model(state)
+        action = torch.argmax(pred)
 
-        prediction = model(state)
-        action = torch.argmax(prediction)
-
-        next_image, reward, terminal = game_state.next_frame(action)
-        next_image = pre_processing(next_image[:game_state.screen_width, :int(game_state.base_y)], image_size,
+        next_image, reward, stop = fb.next_frame(action)
+        next_image = pre_processing(next_image[:fb.screen_width, :int(fb.ground_y)], image_size,
                                     image_size)
         next_image = torch.from_numpy(next_image)
         if torch.cuda.is_available():
@@ -39,10 +38,9 @@ def test_flap(opt):
         next_state = torch.cat((state[0, 1:, :, :], next_image))[None, :, :, :]
 
         state = next_state
-        if terminal:
+        if stop:
             break
 
 
 if __name__ == "__main__":
-    args = get_args()
-    test_flap(args)
+    test_flap()
